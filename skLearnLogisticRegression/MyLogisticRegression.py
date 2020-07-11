45de6febde7433921f675142fcc177431c338f4e
Mikhail <16112000m@gmail.com>
2020-07-11 13:16:46 +0300

    - Тест переобучения

--- skLearnLogisticRegression/MyLogisticRegression.py
+++ skLearnLogisticRegression/MyLogisticRegression.py
@@ -51,3 +51,22 @@ plt.show()
 
 lr.predict_proba(X_test_std[:3, :])
 
+# Настройка переобучения
+
+# weights, params = [], []
+# for c in np.arange(-5, 5):
+#     lr = LogisticRegression(C=10.**c, random_state=1)
+#     lr.fit(X_train_std, y_train)
+#     weights.append(lr.coef_[1])
+#     params.append(10.**c)
+# weights = np.array(weights)
+# plt.plot(params, weights[:, 0],
+#          label='Длина лепестка')
+# plt.plot(params, weights[:, 1], linestyle='--',
+#          label='Ширина лепестка')
+# plt.ylabel('Весовой коэффициент')
+# plt.xlabel('C')
+# plt.legend(loc='upper left')
+# plt.xscale('log')
+# plt.show()
+
